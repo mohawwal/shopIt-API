341d46e6624bd0ca5c669f75d017840907cbb1c1
awwal <leyeawwal@gmail.com>
2024-09-30 03:00:42 +0100

    cors code.

--- app.js
+++ app.js
@@ -7,20 +7,12 @@ const path = require('path');
 
 const app = express();
 
-// Custom CORS middleware
+// Use the CORS package
 app.use(cors({
-    origin: '*', // Adjust this to your specific frontend URL for better security
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
 
 // Middleware for parsing JSON bodies and cookies
 app.use(express.json({ limit: '50mb' }));
@@ -31,21 +23,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'uploads')));
 
 // Import and use routes
-const productsRouter = require('./routes/product')
-app.use('/api/v1', productsRouter)
+const productsRouter = require('./routes/product');
+app.use('/api/v1', productsRouter);
 
-const authRouter = require('./routes/auth')
-app.use('/api/v1', authRouter)
+const authRouter = require('./routes/auth');
+app.use('/api/v1', authRouter);
 
 const orderRouter = require('./routes/order');
-app.use('/api/v1', orderRouter)
+app.use('/api/v1', orderRouter);
 
 const paymentRouter = require('./routes/payment');
-app.use('/api/v1', paymentRouter)
+app.use('/api/v1', paymentRouter);
 
 const fileRouter = require("./routes/file");
 app.use("/api/v1", fileRouter);
-
 
 // Error handling middleware (optional)
 const errorMiddleware = require('./middlewares/errors');

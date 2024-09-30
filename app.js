d1adb4feff5c4faf8aba0a23aec91a28028f649d
awwal <leyeawwal@gmail.com>
2024-09-30 02:57:54 +0100

    cors code

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 
 // Middleware for parsing JSON bodies and cookies
 app.use(express.json({ limit: '50mb' }));
